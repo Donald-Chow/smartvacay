17c8f72cb0a067789e46a8d016415722e5ecdd33
joyce lau <jshizuki@yahoo.com.hk>
2023-05-25 02:10:52 +0900

    Refactored

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,52 +1,84 @@
 class LocationsController < ApplicationController
+  before_action :set_location, only: %i[show favorite]
+
   def index
     @locations = policy_scope(Location)
   end
 
   def show
-    @location = Location.find(params[:id])
     authorize @location
 
     @marker = [{ lat: @location.latitude, lng: @location.longitude }]
-    p "marker: #{@marker}"
+    "marker: #{@marker}"
   end
 
   def favorite
-    @location = Location.find(params[:id])
     authorize @location
     if current_user.favorited?(@location)
       current_user.unfavorite(@location)
-      # flash[:notice] = "Location bookmark removed successfully."
     else
       current_user.favorite(@location)
-      # flash[:notice] = "Location bookmarked successfully."
     end
   end
 
   def my_favorites
     @all_favorites = current_user.all_favorites
-    # Favorite.for_favoritor(current_user) would also work
     authorize @all_favorites
-
-    @category = params[:category]
     @locations = []
 
     @all_favorites.each do |favorite|
+      type_of_place = favorite.favoritable.type_of_place
       location = favorite.favoritable
-      if location.type_of_place.include?("restaurant") || location.type_of_place.include?("meal_takeaway") || location.type_of_place.include?("food")
-        location.category = "food"
-      elsif location.type_of_place.include?("department_store")
-        location.category = "shopping"
-      elsif location.type_of_place.include?("tourist_attraction")
-        location.category = "sightseeing"
-      else
-        location.category = "miscellaneous"
-      end
+      location.category = case
+                          when type_of_place.include?("restaurant") || type_of_place.include?("meal_takeaway") || type_of_place.include?("food")
+                            "food"
+                          when type_of_place.include?("department_store") || type_of_place.include?("shopping_mall")
+                            "shopping"
+                          when type_of_place.include?("tourist_attraction")
+                            "sightseeing"
+                          else
+                            "miscellaneous"
+                          end
       @locations << location
     end
 
-    if @category.present?
-      @locations_by_category = @locations.select { |location| location.category == @category }
+    @category = params[:category]
+
+    @favorites = if @category.present?
+      @locations.select { |location| location.category == @category }
+    else
+      @all_favorites
     end
   end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
 end
+
+# @all_favorites = current_user.all_favorites
+# authorize @all_favorites
+
+# @category = params[:category]
+# @locations = []
+
+# @all_favorites.each do |favorite|
+#   location = favorite.favoritable
+#   if location.type_of_place.include?("restaurant") || location.type_of_place.include?("meal_takeaway") || location.type_of_place.include?("food")
+#     location.category = "food"
+#   elsif location.type_of_place.include?("department_store")
+#     location.category = "shopping"
+#   elsif location.type_of_place.include?("tourist_attraction")
+#     location.category = "sightseeing"
+#   else
+#     location.category = "miscellaneous"
+#   end
+#   @locations << location
+# end
+
+# if @category.present?
+#   @locations_by_category = @locations.select { |location| location.category == @category }
+# end
+# end
