2acc8cb35f89784f801913aa6dcac4765f8caf1b
Arthur Andrade <arthurandrade194@gmail.com>
2023-05-24 18:47:51 +0900

    search working, need to to work in display

--- app/javascript/controllers/google_search_controller.js
+++ app/javascript/controllers/google_search_controller.js
@@ -1,5 +1,5 @@
+// Import the necessary modules
 import { Controller } from "@hotwired/stimulus"
-
 
 // Connects to data-controller="google-search"
 export default class extends Controller {
@@ -8,6 +8,7 @@ export default class extends Controller {
   connect() {
     console.log('Google Search Controller connected');
   }
+
   findPlaceFromQuery(e) {
     e.preventDefault();
     const query = this.inputTarget.value
@@ -24,12 +25,25 @@ export default class extends Controller {
     service.findPlaceFromQuery(request, (results, status) => {
       if (status === google.maps.places.PlacesServiceStatus.OK) {
         console.log(results);
+
+        // Create the card element
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        // Set the card content
+        const name = document.createElement('h3');
+        name.textContent = results[0].name;
+        card.appendChild(name);
+
+        const address = document.createElement('p');
+        address.textContent = results[0].formatted_address;
+        card.appendChild(address);
+
+        // Update the card container with the new card
+        const cardContainer = document.getElementById('card-container');
+        cardContainer.innerHTML = '';
+        cardContainer.appendChild(card);
       }
     });
   }
-
-
-
-
-
 }
