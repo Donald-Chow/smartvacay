a0fbdfeaeed500a343736a7bf6266b8a1e631111
Arthur Andrade <arthurandrade194@gmail.com>
2023-05-31 15:45:15 +0900

    sending email to users upon trip creation

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,7 +3,7 @@
 
 ActionMailer::Base.smtp_settings = {
   address:        'smtp-mail.outlook.com', # default: localhost
-  port:           '25',                  # default: 25
+  port:           '587',                  # default: 25
   user_name:      ENV['OUTLOOK_ADDRESS'],
   password:       ENV['OUTLOOK_APP_PASSWORD'],
   authentication: :login,                 # :plain, :login or :cram_md5
