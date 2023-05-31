96a036f83c5c854d843481cf2e11c81f0bd39729
Arthur Andrade <arthurandrade194@gmail.com>
2023-05-31 15:26:21 +0900

    mailer

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,9 +3,9 @@
 
 ActionMailer::Base.smtp_settings = {
   address:        'smtp-mail.outlook.com', # default: localhost
-  port:           '587',                  # default: 25
+  port:           '25',                  # default: 25
   user_name:      ENV['OUTLOOK_ADDRESS'],
   password:       ENV['OUTLOOK_APP_PASSWORD'],
   authentication: :login,                 # :plain, :login or :cram_md5
-  enable_starttls_auto: true
+  enable_starttls_auto: true,
 }
