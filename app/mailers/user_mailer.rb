50494a34899e0c00ea74934f413071395a33a0be
Arthur Andrade <arthurandrade194@gmail.com>
2023-05-31 16:59:11 +0900

    Ädded Share Button as PDF

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   default from: 'smartvacay@outlook.com'
 
-def welcome
-  @user = params[:user]
-  mail(to: @user.email, subject: 'Thank you for joining SmartVacay')
-  # This will render a view in `app/views/user_mailer`!
-end
+# def welcome
+#   @user = params[:user]
+#   mail(to: @user.email, subject: 'Thank you for joining SmartVacay')
+#   # This will render a view in `app/views/user_mailer`!
+# end
   # To test this code in console you can use the bolow code (assign user to user instance first)
   # UserMailer.with(user: user).welcome(user.email).deliver_now
 
