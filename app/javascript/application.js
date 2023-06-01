3b6831e4bea2fba469f99884c5b4e0b21389aa17
joyce lau <jshizuki@yahoo.com.hk>
2023-06-01 16:05:21 +0900

    Added loading features for home, explore and search

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,15 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+// application.js (or any other JavaScript file)
+
+// Show loading spinner on click
+$(document).on('click', 'a, button, input[type="submit"]', function() {
+  $('#loading-spinner').show();
+});
+
+// Hide loading spinner after page load
+$(window).on('load', function() {
+  $('#loading-spinner').hide();
+});
