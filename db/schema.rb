cd4e9b5a57d0e17b57158e6f80ca3f990f0f5a18
Yin Lee <leesrrh@gmail.com>
2023-06-01 18:29:22 +0900

    add default 0 to rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_053051) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_091532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_053051) do
     t.string "address"
     t.string "phone"
     t.string "website"
-    t.integer "rating"
+    t.integer "rating", default: 0
     t.text "review"
     t.string "photo"
     t.string "opening_hours"
