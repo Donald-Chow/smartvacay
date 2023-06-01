c6ed5778f8a97b552d85b22f3fd70cef8b1e650f
Yin Lee <leesrrh@gmail.com>
2023-06-01 19:37:28 +0900

    disabled = false after 3 seconds of copy clipboard btn

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,15 +3,24 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["input"];
   static values = {
-    feedbackText: String
+    feedbackText: String,
+    originalText: String
   }
 
   copy(event) {
     this.inputTarget.select();
     document.execCommand('copy');
-    event.currentTarget.disabled = true;
-    event.currentTarget.innerText = this.feedbackTextValue;
-    const myTimeout = setTimeout(copy, 5000);
 
+    const button = event.currentTarget;
+    if (button) {
+      button.disabled = true;
+      button.innerText = this.feedbackTextValue;
+
+      setTimeout(() => {
+        button.disabled = false;
+        button.innerText = "Copy to Clipboard";
+      }, 3000);
+    }
   }
+
 }
