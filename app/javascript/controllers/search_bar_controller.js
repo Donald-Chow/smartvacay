3f20cb225b5abe648c5ca3e054f27a9aae383f8b
Donald Chow <donald7290@gmail.com>
2023-06-01 23:50:24 +0900

    search bar can hide

--- app/javascript/controllers/search_bar_controller.js
+++ app/javascript/controllers/search_bar_controller.js
@@ -2,7 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search-bar"
 export default class extends Controller {
+  static targets =["btn", "bar"]
+
   connect() {
-    console.log(searchbox here!);
+    console.log("search bar controller!!!");
+  }
+
+  collapse(event) {
+    console.log("COLLAPSE!");
+    this.barTarget.classList.toggle("active")
+    this.btnTarget.innerHTML="hello"
+    // this.inputTarget.classList.add("active")
   }
 }
