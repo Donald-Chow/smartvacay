606b37f1b684af0f4a730240011668673041a07e
Yin Lee <leesrrh@gmail.com>
2023-06-01 19:40:04 +0900

    minor thing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,11 +6,11 @@ import "bootstrap"
 // application.js (or any other JavaScript file)
 
 // Show loading spinner on click
-$(document).on('click', 'a, button, input[type="submit"]', function() {
-  $('#loading-spinner').show();
-});
+// $(document).on('click', 'a, button, input[type="submit"]', function() {
+//   $('#loading-spinner').show();
+// });
 
 // Hide loading spinner after page load
-$(window).on('load', function() {
-  $('#loading-spinner').hide();
-});
+// $(window).on('load', function() {
+//   $('#loading-spinner').hide();
+// });
